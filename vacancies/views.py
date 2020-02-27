33866930689266ee09abf525333928f255fee9d3
eloc0n <johnisb.21@gmail.com>
2020-02-27 20:25:51 +0200

    added pagination

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,22 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
 
 from .models import Listing, Dutie, Academic, Skill, Benefit
 
 # Create your views here.
 def vacancies(request):
     listings = Listing.objects.order_by('-vacancy_date').filter(is_published=True)
+    
     duties = Dutie.objects.all()
     academics = Academic.objects.all()
     skills = Skill.objects.all()
     benefits = Benefit.objects.all()
 
+    paginator = Paginator(listings, 6)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'listings': listings,
         'duties': duties,
         'academics': academics,
         'skills': skills,
         'benefits': benefits,
+        'listings': page_obj,
     }
 
     return render(request, 'vacancies/vacancies.html', context)
@@ -26,7 +32,7 @@ def vacancy(request, pk):
     vacancy = get_object_or_404(Listing, id=pk)
     
     title_id = Listing.objects.get(id=pk)
-    
+
     duties = title_id.dutie_set.all()
     academics = title_id.academic_set.all()
     skills = title_id.skill_set.all()
